2a0b40d111c21364985933743303ac85d38bbe46
atdow <758815713@qq.com>
2022-05-06 21:24:15 +0800

    chore: 完成1.0功能

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
  * @Author: atdow
  * @Date: 2022-05-04 21:28:49
  * @LastEditors: null
- * @LastEditTime: 2022-05-05 22:34:03
+ * @LastEditTime: 2022-05-06 21:20:23
  * @Description: file description
  */
 const vscode = require('vscode');
@@ -115,7 +115,7 @@ function activate(context) {
             }
         }, undefined, context.subscriptions)
     }))
-    vscode.commands.executeCommand("extension.demo.showPanel")
+    // vscode.commands.executeCommand("extension.demo.showPanel")
 }
 
 function deactivate() { }
