a7efca5cc9f371e5a7bbcf674b9425c9cd0500f5
atdow <758815713@qq.com>
2022-05-09 00:10:00 +0800

    chore: v1.1.0

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@
  * @Author: atdow
  * @Date: 2022-05-04 21:28:49
  * @LastEditors: null
- * @LastEditTime: 2022-05-08 17:02:55
+ * @LastEditTime: 2022-05-09 00:05:48
  * @Description: file description
  */
 const vscode = require('vscode');
@@ -115,7 +115,7 @@ function activate(context) {
             }
         }, undefined, context.subscriptions)
     }))
-    vscode.commands.executeCommand("extension.demo.showPanel")
+    // vscode.commands.executeCommand("extension.demo.showPanel")
 }
 
 function deactivate() { }
